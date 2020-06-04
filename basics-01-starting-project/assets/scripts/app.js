c87bee3d05f1a2b2c65184da50f308f163a19518
naleksankins <naleksankins@evolutiongaming.com>
2020-06-05 00:02:13 +0300

    35 lecture done

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,17 @@
-alert('This works!');
+let currentResult = 0;
+const defaultResult = currentResult;
+let calculationDescription = 'Yankee'
+
+currentResult = currentResult + 10;
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+addBtn.addEventListener('click', add);
+
+outputResult(currentResult, calculationDescription)
